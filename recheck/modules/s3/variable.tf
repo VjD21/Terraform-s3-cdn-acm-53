68c3e0a8910e4af58576129896ac8027ea295283
vijaydurai <vijayduraiaws@gmail.com>
2023-10-09 17:37:34 +0530

    second commit for used s3 policy as a template

---
+++ recheck/modules/s3/variable.tf
@@ -0,0 +1,18 @@
+variable "mys3_bucket_name" {
+
+  type    = string
+  default = "vijaydurai007"
+
+}
+
+variable "mys3_bucket_tag" {
+
+  type    = string
+  default = "vijayduari"
+
+}
+variable "cdn_arn" {
+
+  default = ""
+
+}
